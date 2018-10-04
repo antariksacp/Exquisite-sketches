4571dc63b15f6c338b1333d87a6c454191e71486
Carisa Antariksa <antariksacp@gmail.com>
2018-10-04 16:37:29 -0400

    All initial files for project
    Has all 4 html pages, 4 javascript, 1 css file to position the image.

---
+++ chest-sketch.js
@@ -0,0 +1,18 @@
+var canvas;
+
+function setup(){
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0, 0);
+	strokeWeight(10);
+	stroke(0);
+}
+
+function draw(){
+	if (mouseIsPressed) {
+		line(pmouseX, pmouseY, mouseX, mouseY);
+	}
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+}
